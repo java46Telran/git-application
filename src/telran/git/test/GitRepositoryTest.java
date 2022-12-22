df06ef92057f86e05810fa11cf9dc800aa5efe4a
Yuragranovsky <yuragranovsky@gmail.com>
2022-12-22 21:31:11 +0200

    updated test

--- src/telran/git/test/GitRepositoryTest.java
+++ src/telran/git/test/GitRepositoryTest.java
@@ -3,23 +3,25 @@ package telran.git.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
-import java.net.URI;
 import java.nio.file.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import telran.git.service.*;
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GitRepositoryTest {
 private static final String WORKING_TREE = "working-tree";
 GitRepository gitRepository;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		Path workingDirectory = Path.of(WORKING_TREE);
-		if (!Files.exists(workingDirectory)) {
-			Files.createDirectory(workingDirectory);
-		}
+		Path workingDirectory = Path.of(".");
+		
 		
 		Files.list(workingDirectory).filter(Files::isRegularFile)
 		.forEach(t -> {
@@ -33,8 +35,16 @@ GitRepository gitRepository;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Order(1)
+	@DisplayName("testing methods init, save, adding regex")
+	void initSaveTest() throws Exception {
+		
+		gitRepository = GitRepositoryImpl.init();
+		assertEquals(2, gitRepository.addIgnoredFileNameExp(".*\\.").split("\\|").length);
+		gitRepository.save();
+		gitRepository = GitRepositoryImpl.init();
+		assertEquals(3, gitRepository.addIgnoredFileNameExp(".*\\.").split("\\|").length);
+		
 	}
 
 }
